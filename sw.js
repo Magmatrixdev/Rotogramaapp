db12d6624bccd8255a5a4f15f97bb0c1622c8d2a
Magmatrixdev <pedrosgj@gmail.com>
2026-08-03 23:49:48 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,35 +1,5 @@
-const CACHE_NAME = 'rotograma-v2';
-const ASSETS = [
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-192-maskable.png',
-  './icon-512.png',
-  './icon-512-maskable.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
-});
+const CACHE='rotograma-v3';
+const ASSETS=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{if(e.request.url.includes('firebasedatabase')||e.request.url.includes('googleapis')||e.request.url.includes('openstreetmap')||e.request.url.includes('unpkg'))return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const rc=res.clone();caches.open(CACHE).then(c=>c.put(e.request,rc));return res}).catch(()=>caches.match('./index.html'))))});
