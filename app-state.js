919b4f5c4ab8c11e5487f48de3b487b59b8d282f
Magmatrixdev <pedrosgj@gmail.com>
2026-09-03 14:39:25 -0300

    fix: declare postosAvulsos={} in app-state to prevent TypeError when db is null

--- app-state.js
+++ app-state.js
@@ -10,4 +10,5 @@ let _routeGeometry=[], _routeLayerDone=null, _routeLayerRest=null;
 let db=null, storage=null, firebaseReady=false, isEditing=false, editingIndex=-1, editData={};
 let notifications=[], notifPanelOpen=false;
 let adminMode=false, _cryptoKey=null;
+let postosAvulsos={};
 
